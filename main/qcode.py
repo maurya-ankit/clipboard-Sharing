884f6b67d359adb896b8f85a144cd61768626505
maurya-ankit <ankitmaurya18@cse.iiitp.ac.in>
2020-08-01 11:29:55 +0530

    Added new function in qrcode

--- main/qcode.py
+++ main/qcode.py
@@ -4,3 +4,14 @@ import qrcode
 def qr_img(data):
     img = qrcode.make(data)
     return img
+def adv_qrImg(data):
+    qr=qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    img=qr.make_image(fill_color="black",back_color="white")
+    return img
--- main/run.py
+++ main/run.py
@@ -1,10 +1,9 @@
 import qcode
 import clip
-
 from PIL import Image
 
 data = clip.get_clip()
-img = qcode.qr_img(data)
+img = qcode.adv_qrImg(data)
 #img = Image.open(img)
 
-img.show()
+img.show(title=data)
